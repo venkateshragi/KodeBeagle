73e7030efb6933402369fc183cb01ebf5aa53aa8
Prashant Sharma <scrapcodes@gmail.com>
2015-06-03 15:24:47 +0530

    [infra] Allowing build to break on findbugs and checkstyle violations

---
+++ bin/code_quality.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+use strict;
+
+
+open FILE, "plugins/idea/betterdocsidea/target/findbugs/report.xml" or die "Couldn't open file: $!"; 
+
+my $string = "";
+my $count = -1;
+
+# Findbugs
+while (<FILE>) {
+    $string .= $_;
+    if(/<Errors errors=\"(\d+).*/) {
+        $count = $1;
+    }
+}
+if ($count > 0) {
+    print "ERROR: Found findbug violations.!!\n";
+    print $string;
+    exit(1);
+}
+print "SUCCESS: No findbug violations\n";
+close FILE;
+
+# Checkstyle
+open FILE, "plugins/idea/betterdocsidea/target/checkstyle-result.xml" or die "Couldn't open file: $!"; 
+
+my $string = "";
+my $count = 0;
+
+while (<FILE>) {
+    if(/<error .*/) {
+        $string .= $_;
+        $count = $count + 1;
+    }
+}
+
+if ($count > 0) {
+    print "ERROR: Found checkstyle violations.!!\n";
+    print $string;
+    exit(1);
+}
+
+print "SUCCESS: No checkstyle violations\n";
+close FILE;
